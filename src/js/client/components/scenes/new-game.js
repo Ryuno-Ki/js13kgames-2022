515cd07b69f280ffd6273218fccc460e35d5c86b
André Jaenisch <andre.jaenisch@posteo.de>
2022-08-14 19:33:57 +0200

    Define rudimentary level
    This one already has a path, tower positions and enemies moving
    along the path. Next thing is thinking about how the tower and
    enemies interact.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/client/components/scenes/new-game.js
+++ src/js/client/components/scenes/new-game.js
@@ -4,8 +4,63 @@ export function newGameSceneComponent (targetElement, state) {
   if (state.activeScene !== 'new-game-scene') {
     element.innerHTML = '';
   } else {
-	  element.innerHTML = 'New game scene';
+    const level = state.levels[ state.activeLevel ];
+    const { enemies, mode, pathway, towers } = level;
+
+    const height = 320;
+    const width = 320;
+
+    const fill = mode === 'death' ? 'black' : 'white';
+    const stroke = mode === 'death' ? 'white' : 'black';
+    const path = mapCoordinatesToPath(pathway);
+
+    element.innerHTML = `
+      <svg
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid meet"
+        xmlns="http://www.w3.org/2000/svg"
+        height="${height}"
+        width="${width}"
+        aria-labelledby="level-title"
+        aria-describedby="level-desc"
+      >
+        <title id="level-title"></title>
+        <desc id="level-desc"></desc>
+        <style>
+          text {
+            font-size: 0.5em;
+          }
+        </style>
+
+        <g id="level">
+          <rect x="0" y="0" height="${height}" width="${width}" fill="${fill}" />
+          <g id="pathway">
+            <path d="${path}" stroke="${stroke}" strokeWidth="6" fill="none" />
+          </g>
+          <g id="enemies">
+            <text>
+              <textPath path="${path}">
+                  ${enemies}
+                <animate attributeName="startOffset" from="-100%" to="200%" begin="0s" dur="5s" repeatCount="1" />
+              </textPath>
+            </text>
+          </g>
+          <g id="towers">
+            ${towers.map((tower) => {
+              return `<circle cx="${tower.position[0]}" cy="${tower.position[1]}" r="2" fill="${stroke}" />`;
+            }).join('')}
+          </g>
+        </g>
+      </svg>
+    `;
   }
 
 	return element;
+}
+
+function mapCoordinatesToPath (coordinates) {
+  return [
+      `M${coordinates[0].join(',')}`,
+      coordinates.slice(1).map((segment) => `L${segment.join(',')}`).join(''),
+    ].join('');
 }
--- src/js/client/state/reducers/index.js
+++ src/js/client/state/reducers/index.js
@@ -2,7 +2,27 @@ import { ACTION_NAVIGATE_TO_SCENE } from '../../../shared/constants.js';
 import { navigateToScene } from './navigate-to-scene.js';
 
 const initialState = {
+  activeLevel: 0,
   activeScene: 'title-scene',
+  levels: [{
+    mode: 'death',
+    enemies: '💀👺👹👻',
+    pathway: [
+      [   0,  50 ],
+      [  30,  50 ],
+      [  30,  70 ],
+      [  70,  70 ],
+      [  70,  50 ],
+      [ 100,  50 ]
+    ],
+    towers: [{
+      position: [  30,  44 ],
+      type: null,
+    }, {
+      position: [  36,  50 ],
+      type: null,
+    }],
+  }],
 };
 
 export function reducer (state, action) {
