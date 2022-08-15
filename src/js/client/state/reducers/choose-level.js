d4d395cea9da275481dc0d8ccc9d8c7656edea2d
André Jaenisch <andre.jaenisch@posteo.de>
2022-08-15 19:00:54 +0200

    Parse activeLevel as number
    Usually, DOM form elements pass values as strings.
    Perhaps that's what caused those weird observations.
    
    However, parsing can fail, so I set a fallback value in those cases.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/client/state/reducers/choose-level.js
+++ src/js/client/state/reducers/choose-level.js
@@ -1,5 +1,10 @@
 export function chooseLevel (state, payload) {
-  const { level: activeLevel } = payload;
+  const { level } = payload;
+  let activeLevel = parseInt(level, 10);
+
+  if (isNaN(activeLevel)) {
+    activeLevel = null;
+  }
 
   return Object.assign({}, state, { activeLevel });
 }
