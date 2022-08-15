80a609ecea4b6d92db77fc9fdd4a1f3ad9b32066
André Jaenisch <andre.jaenisch@posteo.de>
2022-08-15 22:38:45 +0200

    Demark begin and end of pathway
    In order to have an affordance about the direction of the pathway
    demark begin and end of it.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/client/components/pathway.js
+++ src/js/client/components/pathway.js
@@ -8,8 +8,33 @@ export function pathwayComponent (targetElement, state) {
   const path = mapCoordinatesToPath(pathway);
 
   element.innerHTML = `
+    ${getStartingPoint(pathway)}
     <path d="${path}" stroke="${stroke}" strokeWidth="6" fill="none" />
+    ${getEndpoint(pathway)}
   `;
 
   return element;
+}
+
+function getStartingPoint (pathway) {
+  if (pathway.length === 0) {
+    return '';
+  }
+
+  // TODO: Depends on level mode!
+  const begin = '🪦';
+  const [ beginX, beginY ] = pathway[ 0 ];
+  return `<text x="${beginX}" y="${beginY}">${begin}</text>`;
+}
+
+function getEndpoint (pathway) {
+  if (pathway.length < 2) {
+    return '';
+  }
+
+  // TODO: Depends on level mode!
+  const fin = '🛐';
+  const [ finX, finY ] = pathway[ pathway.length - 1 ];
+
+  return `<text x="${finX}" y="${finY}">${fin}</text>`;
 }
