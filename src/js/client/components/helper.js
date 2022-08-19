32efd0b86e790606ce22c917e76f880e6d5fee14
André Jaenisch <andre.jaenisch@posteo.de>
2022-08-19 20:22:57 +0200

    Store reference to circle collision detection
    My battery is dying, so I need to update it for my use case later.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/client/components/helper.js
+++ src/js/client/components/helper.js
@@ -16,3 +16,22 @@ export function mapCoordinatesToPath (coordinates) {
       coordinates.slice(1).map((segment) => `L${segment.join(',')}`).join(''),
     ].join('');
 }
+
+// Kudos: https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection#circle_collision
+export function detectCircleCollision () {
+  /*
+   * circle2.bind("EnterFrame", () => {
+    const dx = circle1.x - circle2.x;
+    const dy = circle1.y - circle2.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance < circle1.radius + circle2.radius) {
+        // collision detected!
+        this.color = "green";
+    } else {
+        // no collision
+        this.color = "blue";
+    }
+});
+*/
+}
