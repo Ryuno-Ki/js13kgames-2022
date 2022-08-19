ac13f0c526c7a321b904b099e27ea6d5d1b52e39
André Jaenisch <andre.jaenisch@posteo.de>
2022-08-19 19:35:02 +0200

    Refactor animation
    It jumps between line segments, albeit not yet smoothly.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/client/state/reducers/update-positions.js
+++ src/js/client/state/reducers/update-positions.js
@@ -22,21 +22,7 @@ export function updatePositions (state) {
 }
 
 function computeNewPosition ({ enemy, pathway }) {
-	const { begin, speed } = enemy;
-  const now = new Date() - 0;
-  const sinceBegin = now - begin;
-
-  // TODO: The math feels too complicated. Get some pen and paper
-  // Dur in seconds, sinceBegin is in milliseconds
-  const dur = speed * 1000;
-  // First divide to seconds, then to get a percentage
-  const t = clamp(
-    sinceBegin / (dur + begin),
-    sinceBegin,
-    sinceBegin + dur
-  ) / 1000 / speed;
-
-  const [ start, end ] = getLineSegment(pathway);
+  const [ start, end, t ] = getLineSegment(enemy, pathway);
   const [ x0, y0 ] = start;
   const [ x1, y1 ] = end;
 
@@ -44,6 +30,34 @@ function computeNewPosition ({ enemy, pathway }) {
   const y = clamp((1 - t) * y0 + t * y1, y0, y1);
 
   return [ x, y ];
+}
+
+function getLineSegment (enemy, pathway) {
+  const now = new Date() - 0;
+  const progressInTime = getProgressInTime(now, enemy);
+  const progressInSpace = getProgressInSpace(pathway);
+
+  const i = clamp(
+    getLineSegmentIndex(progressInTime, progressInSpace),
+    0,
+    pathway.length - 2
+  );
+
+  const startIndex = i;
+  const endIndex = i + 1;
+  const start = pathway[ startIndex ];
+  const end = pathway[ endIndex ];
+
+  const x0 = parseFloat(start[0]);
+  const y0 = parseFloat(start[1]);
+  const x1 = parseFloat(end[0]);
+  const y1 = parseFloat(end[1]);
+
+  return [
+    [ x0, y0 ],
+    [ x1, y1 ],
+    progressInTime,
+  ];
 }
 
 function clamp (current, lower, upper) {
@@ -58,16 +72,55 @@ function clamp (current, lower, upper) {
   return current;
 }
 
-// TODO: Line segment depends on elapsed time and enemy.speed
-function getLineSegment (pathway) {
-  const [ start, end ] = pathway;
-  const x0 = parseFloat(start[0]);
-  const y0 = parseFloat(start[1]);
-  const x1 = parseFloat(end[0]);
-  const y1 = parseFloat(end[1]);
+function getProgressInTime (now, enemy) {
+  const { begin, speed } = enemy;
+  const dur = speed * 1000; // in ms
 
-  return [
-    [ x0, y0 ],
-    [ x1, y1 ],
-  ];
+  const t = clamp(now - begin, 0, dur);
+
+  return t / dur;
+}
+
+function getProgressInSpace (pathway) {
+  const progressInSpace = [];
+  const totalPathwayLength = getTotalPathwayLength(pathway);
+  let sum = 0;
+
+  for (let i = 0; i < pathway.length - 1; i++) {
+    const [ x, y ] = pathway[ i ];
+    const lineSegmentLength = Math.sqrt(x * x + y * y);
+
+    progressInSpace.push(sum + lineSegmentLength / totalPathwayLength);
+    sum = progressInSpace[ progressInSpace.length - 1 ];
+  }
+
+  return [ 0 ].concat(progressInSpace);
+}
+
+function getTotalPathwayLength (pathway) {
+  const lengths = pathway.map((point, index, arr) => {
+    if (index === arr.length - 1) {
+      return null;
+    }
+
+    const [ x, y ] = point;
+    return Math.sqrt(x * x + y * y);
+  }).filter(Boolean);
+
+  return lengths.reduce((sum, len) => sum + len, 0);
+}
+
+function getLineSegmentIndex (time, space) {
+  let index = 0;
+
+  for (let i = space.length - 1; i >= 0; i--) {
+    const treshold = space[ i ];
+
+    if (time > treshold) {
+      index = i;
+      break;
+    }
+  }
+
+  return index;
 }
