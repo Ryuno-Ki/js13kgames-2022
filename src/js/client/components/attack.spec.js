81aed79817f6d7294a9b545c5842643b753cb5f2
André Jaenisch <andre.jaenisch@posteo.de>
2022-09-06 19:22:45 +0200

    Write test for presence and shape of buttons
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/client/components/attack.spec.js
+++ src/js/client/components/attack.spec.js
@@ -10,21 +10,37 @@ chai.use(chaiDom);
 const { expect } = chai;
 
 function prepareDocument () {
-	const jsdom = new JSDOM('');
-	return jsdom.window.document;
+  const jsdom = new JSDOM('');
+  return jsdom.window.document;
 }
 
 describe('attackComponent', () => {
-	it('should render a component', () => {
-	  // Arrange
-	  const document = prepareDocument();
-	  const targetElement = document.createElement('div');
-		const state = Object.assign({}, initialState, { activeLevel: 1 });
+  it('should render a component', () => {
+    // Arrange
+    const document = prepareDocument();
+    const targetElement = document.createElement('div');
+    const state = /** @type {import('../state/reducers/index.js').State} */(Object.assign({}, initialState, { activeLevel: 1 }));
 
-	  // Act
-	  const component = attackComponent(targetElement, state);
+    // Act
+    const component = attackComponent(targetElement, state);
 
-	  // Assert
-	  expect(component).to.have.tagName('div');
-	});
+    // Assert
+    expect(component).to.have.tagName('div');
+  });
+
+  it('should render buttons', () => {
+    // Arrange
+    const document = prepareDocument();
+    const targetElement = document.createElement('div');
+    const state = /** @type {import('../state/reducers/index.js').State} */(Object.assign({}, initialState, { activeLevel: 1 }));
+    const mode = state.levels[ 0 ].mode;
+    const enemies = state.entities[ mode ];
+
+    // Act
+    const component = attackComponent(targetElement, state);
+
+    expect(component).to.have.descendants('button').and.have.length(enemies.length);
+    expect(component.querySelector('button')).to.have.attribute('type', 'button');
+    expect(component.querySelector('button')).to.have.attribute('data-add-entity');
+  });
 });
